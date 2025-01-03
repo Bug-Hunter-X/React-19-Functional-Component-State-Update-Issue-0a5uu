2e4bdcb86a23f5714668cc77724c81dd8eb1d7af
BugHunter-X <askzenai@gmail.com>
2025-01-03 14:00:04 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,13 @@
+```javascript
+// Incorrect usage of functional component causing unexpected behavior in React 19
+function MyComponent() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  );
+}
+```
