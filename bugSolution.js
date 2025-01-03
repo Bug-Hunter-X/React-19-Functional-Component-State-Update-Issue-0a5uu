5801b96dc785e0f1d7d5d44c0e6e2b9be5621251
BugHunter-X <askzenai@gmail.com>
2025-01-03 14:00:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,17 @@
+```javascript
+// Correct usage of functional component in React 19
+import React, { useState } from 'react';
+
+function MyComponent() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(prevCount => prevCount + 1)}>Click me</button>
+    </div>
+  );
+}
+
+export default MyComponent;
+```
